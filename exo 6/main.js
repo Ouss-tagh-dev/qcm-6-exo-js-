db8deed584da39b5dd30eb65937016c2a8e1278d
ouss-tagh-dev <oussamataghlaoui20@gmail.com>
2022-12-03 16:49:26 +0100

    ajouter le code de decrementer

--- exo 6/main.js
+++ exo 6/main.js
@@ -4,8 +4,10 @@ spanSecond = document.getElementById("spSecond");
 let initTime = { heure: 10, minute: 23, second: 5 };
 let currentTime = initTime;
 let timer = null;
+
+// incrementer
 function incrementer(temps) {
-  time = { ...temps };
+  var time = { ...temps };
   return function () {
     time.second++;
     if (time.second >= 60) {
@@ -21,19 +23,52 @@ function incrementer(temps) {
     return time;
   };
 }
+
+// decrementer
+function decrementer(temps) {
+  tim = { ...currentTime };
+  return function () {
+    tim.second--;
+    if (tim.second == 0) {
+      tim.second = 60;
+      tim.minute--;
+      if (tim.minute == 0) {
+        tim.minute = 60;
+        tim.heure--;
+      }
+    }
+    affiche(tim);
+    currentTime = tim;
+    return tim;
+  };
+}
+
+// affiche
 function affiche(time) {
   spanHeure.innerHTML = time.heure;
   spanMinute.innerHTML = time.minute;
   spanSecond.innerHTML = time.second;
 }
+
+// stopTimer
 function stopTimer() {
   if (timer) {
     clearInterval(timer);
   }
   timer = null;
 }
+
+// incrementerTimer
 function incrementerTimer() {
   stopTimer();
   let next = incrementer(currentTime);
   timer = setInterval(next, 1000);
 }
+
+// decrementerTimer
+function decrementerTimer() {
+  stopTimer();
+  let pre = decrementer(currentTime);
+  timer = setInterval(pre, 1000);
+}
+
