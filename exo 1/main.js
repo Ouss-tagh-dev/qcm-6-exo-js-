2c315aa950bbc3be567f224b4d3791e0042367a9
ouss-tagh-dev <oussamataghlaoui20@gmail.com>
2022-12-03 18:39:36 +0100

    L'ajout des commentaires

--- exo 1/main.js
+++ exo 1/main.js
@@ -1,3 +1,4 @@
+// Créer la classe Document contenant les attributs id et dateEdition
 class Document {
     constructor(id, dateEdition) {
       this.id = id;
@@ -6,7 +7,7 @@ class Document {
   }
 
 
-
+// Créer la classe Livre qui hérite de la classe Document contenant en plus les attributs titre et auteur
 class Livre extends Document {
     constructor(id,dateEdition, titre, auteur) {
       super(id, dateEdition );
@@ -14,14 +15,17 @@ class Livre extends Document {
       this.auteur = auteur;
     }
 
+    // Créer la méthode infoLivre qui retourne les informations du livre
     infoLivre() {
         return  `Livre: id:${this.id} titre:${this.titre} auteur:${this.auteur} dateEdition:${this.dateEdition}`;
     }
 }
 
+// Créer deux instances de Document
 d1 = new Document(1, '01/11/2001');
 d2 = new Document(2, '12/12/2012');
 
+// Créer trois instances de Livre
 l1 = new Livre(1, '01/11/2001', 'Livre 1', 'Auteur 1');
 l2 = new Livre(2, '02/12/2010', 'Livre 2', 'Auteur 2');
 l3 = new Livre(3, '10/15/2021', 'Livre 3', 'Auteur 3');
