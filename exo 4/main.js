90ebdffdd50a9078dd717dcc281bc3dd7dc25e9b
ouss-tagh-dev <oussamataghlaoui20@gmail.com>
2022-12-03 18:31:20 +0100

    Mise a jour le code js de exo 4

--- exo 4/main.js
+++ exo 4/main.js
@@ -1,15 +1,12 @@
 // Soit le tableau suivant : 
 const input = [1, -2, 3, 4, -5]
 
+// Utiliser  la  méthode  filter  pour  créer  un  tableau  contenant  les  éléments  positifs  de nums 
 console.log(input.filter(n=>n > 0))
-
 
 // Utiliser la méthode reduce pour calculer la somme des éléments de nums  
 console.log(input.reduce(n=(t,n)=>t+n , 0)) // 1
 
-console.log("Test")
-// Utiliser  la  méthode  filter  et  reduce  pour  calculer  la  somme des  éléments  positifs  de nums 
-t = []
-t.push(input.filter(n=>n > 0))
-console.log(t)
-console.log(t.reduce(n=(t,n)=>t+n , 0))
+// Utiliser la méthode  filter  et  reduce  pour  calculer  la  somme des  éléments  positifs  de nums 
+console.log(input.filter(n=>n > 0).reduce(n=(t,n)=>t+n, 0))
+
