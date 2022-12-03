66122218c66ee89b70ed7745840d4314ef3f1ba1
ouss-tagh-dev <oussamataghlaoui20@gmail.com>
2022-12-03 18:16:56 +0100

    update exo 3

---
+++ exo 3/main.js
@@ -0,0 +1,6 @@
+// Soit le tableau suivant
+const input = [1, 2, 3, 4, 5] 
+
+// Utiliser  la  méthode  map  pour  créer  un  tableau  contenant  le  carré  des  éléments  de nums
+console.log(input.map(n=>n*n))
+
